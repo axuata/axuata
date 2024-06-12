7a07af3896bb70e8d327da180fa2f19dab8191f7
Axuata <axuata@gmail.com>
2024-06-12 14:16:41 +0900

    add: ヘッダーを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,13 +7,17 @@ export default defineNuxtConfig({
   modules: [ '@nuxt/image'],
   app:{
     head: {
-      title: "サイト名をここに",
+      title: "Axuata",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
       link: [
-        { rel: "icon", type: "image/png", href: "/favicon.png" }, // ファビコンは public/favicon.png にしてください
+        { rel: "icon", type: "image/png", href: "/axuata.svg" },
+        { rel: "stylesheet", href: "https://unpkg.com/modern-css-reset/dist/reset.min.css" },
+      ],
+      script: [
+        { src: "https://kit.fontawesome.com/e48ae4e274.js", crossorigin: "anonymous"},
       ],
     },
     pageTransition: { name: 'page', mode: 'out-in' },
