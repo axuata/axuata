e8a0ba1787fa61d37d8b3c9e36121fe5d311727f
Axuata <axuata@gmail.com>
2024-06-13 11:24:08 +0900

    add: 「主なプロジェクト」セクションを「ホーム」に追加

--- components/global/layout/GlobalLayoutFooter.vue
+++ components/global/layout/GlobalLayoutFooter.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-
+  <div class="footer">
+    <p>© 2024 Axuata - All Rights Reserved.</p>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.footer {
+  p {
+    padding: 16px;
+    text-align: center;
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-  
+  <GlobalWelcomeSection titleText="ようこそ" descriptionTextHtml="<p>Axuataのサイトへようこそ<br>このサイトではAxuataの活動内容をまとめています。</p>" />
+  <GlobalSectionWithSlot titleText="主なプロジェクト">
+    <GlobalCard titleText="Axuata（ウェブサイト）" descriptionTextHtml="<p>Axuataのサイト。<br><a href='https://github.com/axuata/axuata'>Github</a></p>" />
+    <GlobalCard titleText="Accents" descriptionTextHtml="<p>自由にアクセント記号を付けれるソフト。<br><a href='https://github.com/axuata/accents'>Github</a></p>" />
+  </GlobalSectionWithSlot>
 </template>
