30c30644aee6056b713bb652ff46079a2349b7d2
Axuata <axuata@gmail.com>
2024-08-09 16:51:29 +0900

    Edit: プロジェクトリストを最新化

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,5 @@
   <GlobalWelcomeSection titleText="ようこそ" descriptionTextHtml="<p>Axuataのサイトへようこそ<br>このサイトではAxuataの活動内容をまとめています。</p>" />
   <GlobalSectionWithSlot titleText="主なプロジェクト">
     <GlobalCard titleText="Axuata（ウェブサイト）" descriptionTextHtml="<p>Axuataのサイト。<br><a href='https://github.com/axuata/axuata'>Github</a></p>" />
-    <GlobalCard titleText="Japan Railway Map" descriptionTextHtml="<p>日本鉄道マップ<br><a href='https://github.com/axuata/japan-railway-map'>Github</a></p>" />
   </GlobalSectionWithSlot>
 </template>
