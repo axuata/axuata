e6928b54d9ebc5d86e5140e80b375182f0ae86a2
Axuata <axuata@gmail.com>
2024-12-14 14:23:12 +0900

    プロジェクトのカードを変更

---
+++ components/common/CInfoCard.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+export type Links = {
+  [name: string]: {
+    iconSrc: string;
+    link: string;
+  };
+};
+
+const props = defineProps({
+  logo: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  websiteLink: {
+    type: String,
+    required: false,
+  },
+  links: {
+    type: Object as () => Links,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <div class="bg-white w-460px b-solid b-1px b-gray-2 p-10px rounded-14px my-5px">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center ml-2px">
+        <div>
+          <img class="size-20px rounded-7px" :src="logo" :alt="logo" />
+        </div>
+        <div>
+          <a v-if="websiteLink" class="font-inter text-gray-8 font-600 ml-4px" :href="websiteLink">{{ title }}</a>
+          <p v-else class="font-inter text-gray-8  font-600 ml-4px">{{ title }}</p>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div class="flex items-center ml-10px" v-for="(link, name) in links" :key="name">
+          <a class="flex items-center" :href="link.link">
+            <img class="size-16px mr-6px" :src="link.iconSrc" :alt="link.iconSrc" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center mt-5px">
+      <p class="font-inter text-gray-7">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,23 +1,51 @@
 <script setup lang="ts">
+import CInfoCard, { type Links } from '~/components/common/CInfoCard.vue';
 
+const axuataLinks: Links = {
+  github: {
+    iconSrc: '/images/icons/brand/github.svg',
+    link: 'https://github.com/axuata/axuata'
+  }
+}
+
+const nuxtlyLinks: Links = {
+  github: {
+    iconSrc: '/images/icons/brand/github.svg',
+    link: 'https://github.com/axuata/nuxtly'
+  }
+}
+
+const vanillaPlusLinks: Links = {
+  github: {
+    iconSrc: '/images/icons/brand/github.svg',
+    link: 'https://github.com/axuata/vanilla_plus'
+  },
+}
+
+const assetsLinks: Links = {
+  github: {
+    iconSrc: '/images/icons/brand/github.svg',
+    link: 'https://github.com/axuata/assets'
+  }
+}
 </script>
 
 <template>
   <CTitle title="Projects" description="Axuataのプロジェクト" />
   <CSection title="Web" description="ウェブ関係のプロジェクト" header-align="center" title-font="inter">
     <div class="flex flex-col items-center my-10px">
-      <CCard logo="/images/logos/axuata/project-logos/axuata/logo.svg" title="Axuata" description="🌍 Axuata's Personal Website" link="https://github.com/axuata/axuata" />
-      <CCard logo="/images/logos/axuata/project-logos/nuxtly/logo.svg" title="Nuxtly" description="📄 Nuxt Starter by Axuata" link="https://github.com/axuata/nuxtly" />
+      <CInfoCard logo="/images/logos/axuata/project-logos/axuata/logo.svg" title="Axuata" description="🌍 Axuata's Personal Website" website-link="https://axuata.netlify.app" :links="axuataLinks" />
+      <CInfoCard logo="/images/logos/axuata/project-logos/nuxtly/logo.svg" title="Nuxtly" description="📄 Nuxt Starter by Axuata" :links="nuxtlyLinks" />
     </div>
   </CSection>
   <CSection title="Mods" description="ゲームのMOD" header-align="center" title-font="inter">
     <div class="flex flex-col items-center my-10px">
-      <CCard logo="/images/logos/axuata/project-logos/vanilla_plus/logo.svg" title="Vanilla Plus (開発中)" description="⚔️ Vanilla Plus mod for Hearts of Iron 4" link="https://github.com/axuata/vanilla_plus" />
+      <CInfoCard logo="/images/logos/axuata/project-logos/vanilla_plus/logo.svg" title="Vanilla Plus" description="⚔️ Vanilla Plus mod for Hearts of Iron 4" :links="vanillaPlusLinks" />
     </div>
   </CSection>
   <CSection title="Others" description="その他" header-align="center" title-font="inter">
     <div class="flex flex-col items-center my-10px">
-      <CCard logo="/images/logos/axuata/project-logos/assets/logo.svg" title="Assets" description="🗂️ Static Assets Repository by Axuata" link="https://github.com/axuata/assets" />
+      <CInfoCard logo="/images/logos/axuata/project-logos/assets/logo.svg" title="Assets" description="🗂️ Static Assets Repository by Axuata" :links="assetsLinks" />
     </div>
   </CSection>
 </template>
