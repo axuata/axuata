e3458df8ec2fa399d74a2085476cffc846f31db5
Axuata <axuata@gmail.com>
2024-12-14 11:19:40 +0900

    Welcome を改良 ちょっとよくなった

--- components/page-specific/home/CWelcome.vue
+++ components/page-specific/home/CWelcome.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-200px text-center">
-    <p class="font-inter font-600 text-50px text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">I am Axuata</p>
-    <p class="font-inter font-500 text-gray-5 text-18px">I am Axuata, I am from Japan. Call me Yottaka or Axuata!</p>
+  <div class="h-500px flex flex-col items-center justify-center text-center">
+    <div class="font-inter flex font-600 text-80px">
+      <p class="text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">I am&nbsp;</p>
+      <p class="text-transparent bg-clip-text bg-gradient-to-b from-sky-3 to-sky-5">Axuata</p>
+    </div>
+    <p class="font-inter font-500 text-gray-6 text-24px">I am Axuata, I am from <Twemoji emoji="🇯🇵" /> Japan. Call me Yottaka or Axuata!</p>
   </div>
 </template>
 
