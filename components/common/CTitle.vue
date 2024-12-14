90a48b44d23df79df9b34ebeb0e8f59841d208a4
Axuata <axuata@gmail.com>
2024-12-14 11:21:04 +0900

    TitleのフォントをInterに変更 これでもっと読みやすくなった

--- components/common/CTitle.vue
+++ components/common/CTitle.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 
 <template>
   <div class="flex flex-col items-center justify-center h-200px text-center">
-    <p class="font-notosansjp font-600 text-50px text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">{{ title }}</p>
+    <p class="font-inter font-600 text-50px text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">{{ title }}</p>
     <p class="font-notosansjp font-500 text-gray-5 text-18px">{{ description }}</p>
   </div>
 </template>
