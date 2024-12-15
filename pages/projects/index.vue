a4adfb4700ef4b00a749959713f9b46a3f0075a2
Axuata <axuata@gmail.com>
2024-12-15 16:26:46 +0900

    Vanilla Plusを削除（非公開になったため）

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -10,11 +10,6 @@
       <CInfoCard logo="/images/logos/axuata/project-logos/nuxtly/logo.svg" title="Nuxtly" description="📄 Nuxt Starter by Axuata" link="https://github.com/axuata/nuxtly" />
     </div>
   </CSection>
-  <CSection title="Mods" description="ゲームのMOD" header-align="center" title-font="inter">
-    <div class="flex flex-col items-center my-10px">
-      <CInfoCard logo="/images/logos/axuata/project-logos/vanilla_plus/logo.svg" title="Vanilla Plus" description="⚔️ Vanilla Plus mod for Hearts of Iron 4" link="https://github.com/axuata/vanilla_plus" />
-    </div>
-  </CSection>
   <CSection title="Others" description="その他" header-align="center" title-font="inter">
     <div class="flex flex-col items-center my-10px">
       <CInfoCard logo="/images/logos/axuata/project-logos/assets/logo.svg" title="Assets" description="🗂️ Static Assets Repository by Axuata" link="https://github.com/axuata/assets" />
