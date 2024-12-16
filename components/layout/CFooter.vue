cd229aec76954b0cb8c7881ba9e685b0b757d266
Axuata <axuata@gmail.com>
2024-12-16 17:51:34 +0900

    ライセンス表記を修正

--- components/layout/CFooter.vue
+++ components/layout/CFooter.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="font-inter font-500 text-gray-6 flex items-center justify-center w-full h-80px">
-    <p>© 2024 - 2024 Axuata - All Rights Reserved.</p>
+    <p>&copy; 2024 Axuata. This project is licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.</p>
   </div>
 </template>
 
