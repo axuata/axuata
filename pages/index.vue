1ef57839f66a8bc066515a51297f46300bfa964f
Axuata <axuata@gmail.com>
2024-12-18 13:17:56 +0900

    ホームのPostsにアイコン変更のお知らせを追加

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,8 @@
 <!--  <CAbout />-->
   <CSection title="Posts" description="Axuataによる投稿" header-align="center" title-font="inter">
     <div class="flex justify-center my-10px">
-      <CPostCard image-src="/images/thumbnails/posts/news/website-published.png" title="公開" date="2024/12/14" href="/posts/news/website-published" />
+      <CPostCard image-src="/images/thumbnails/posts/news/icon-change.png" title="アイコン変更のお知らせ" date="2024/12/18" href="/posts/news/icon-change/" />
+      <CPostCard image-src="/images/thumbnails/posts/news/website-published.png" title="公開" date="2024/12/14" href="/posts/news/website-published/" />
     </div>
   </CSection>
 </template>
