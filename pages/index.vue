50a846d3539b70ba696fd4745b1a7f738f60f69b
Axuata <axuata@gmail.com>
2024-12-19 18:06:07 +0900

    ファイル名を最適化

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
 <!--  <CAbout />-->
   <CSection title="Posts" description="Axuataによる投稿" header-align="center" title-font="inter">
     <div class="flex justify-center my-10px">
-      <CPostCard image-src="/images/thumbnails/posts/news/icon-change.png" title="アイコン変更のお知らせ" date="2024/12/18" href="/posts/news/icon-change/" />
-      <CPostCard image-src="/images/thumbnails/posts/news/website-published.png" title="公開" date="2024/12/14" href="/posts/news/website-published/" />
+      <CPostCard image-src="/images/thumbnails/posts/news/2024-12-18-icon-change.png" title="アイコン変更のお知らせ" date="2024/12/18" href="/posts/news/2024-12-18-icon-change/" />
+      <CPostCard image-src="/images/thumbnails/posts/news/2024-12-14-website-published.png" title="公開" date="2024/12/14" href="/posts/news/2024-12-14-website-published/" />
     </div>
   </CSection>
 </template>
