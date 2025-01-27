db0b1c98e4d9027a1670517d4b1a8bafe1e7f98e
Axuata <axuata@gmail.com>
2025-01-27 11:34:28 +0900

    日付を修正

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -6,7 +6,7 @@
   <CWelcome title="Posts" description="Axuataによる投稿" />
   <CSection title="News" description="お知らせ">
     <div class="flex flex-wrap justify-center">
-      <CCard link="/posts/news/2025-01-02/" emoji="🌅" title="明けましておめでとうございます！" description="あけおめ！" date="2025/01/27" />
+      <CCard link="/posts/news/2025-01-02/" emoji="🌅" title="明けましておめでとうございます！" description="あけおめ！" date="2025/01/02" />
     </div>
   </CSection>
 </template>
