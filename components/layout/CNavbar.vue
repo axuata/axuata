5e8156536976b906da1e74c97dfe495f449920d8
Axuata <axuata@gmail.com>
2025-01-27 10:36:33 +0900

    assetsを実際のディレクトリ構造に変更

--- components/layout/CNavbar.vue
+++ components/layout/CNavbar.vue
@@ -6,7 +6,7 @@
   <div class="flex items-center justify-center w-full">
     <div class="flex items-center justify-between max-w-1200px w-full h-60px">
       <NuxtLink to="/" class="flex items-center justify-center">
-        <img src="/images/logos/banner/banner.png" alt="/images/logos/banner/banner.png" class="h-55px" />
+        <img src="/images/icons/banner/banner.png" alt="/images/icons/banner/banner.png" class="h-55px" />
       </NuxtLink>
       <div class="font-inter flex items-center justify-center *:mx-3px">
         <NuxtLink to="/">Home</NuxtLink>
--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     <CSection title="About Me" description="私について">
       <div class="flex flex-wrap items-center justify-start mx-auto w-600px h-140px bg-white b-solid b-gray-2 b-1px rounded-16px my-8px">
         <div class="b-r-dashed b-r-gray-3 b-r-1px">
-          <img src="/images/logos/logo.png" alt="/images/logos/logo.png" class="size-140px" />
+          <img src="/images/icons/icon.png" alt="/images/icons/icon.png" class="size-140px" />
         </div>
         <div class="ml-20px">
           <div>
