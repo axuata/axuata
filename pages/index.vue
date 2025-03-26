31e2195dbdeb27c494c9c5531de32d4c6260a6f5
Axuata <axuata@gmail.com>
2025-03-26 19:12:11 +0900

    Added Real-time greet and Real-time clock

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
+const greet = ref<string>('Welcome!');
+const clock = ref<string>('XX:XX:XX');
+let clockInterval: ReturnType<typeof setTimeout>;
 
+onMounted(() => {
+  const date: Date = new Date() as Date;
+  const currentHour: number = date.getHours() as number;
+
+  clockInterval = setInterval(() => {
+    const dateForClock: Date = new Date() as Date;
+    clock.value = `${dateForClock.getHours().toString().padStart(2, '0')}:${dateForClock.getMinutes().toString().padStart(2, '0')}:${dateForClock.getSeconds().toString().padStart(2, '0')}`;
+  }, 500);
+
+  if (date && currentHour) {
+    if (currentHour >= 5 && currentHour < 10) {
+      greet.value = 'Good morning!';
+    } else if (currentHour >= 10 && currentHour <= 17) {
+      greet.value = 'Hello!';
+    } else if (currentHour >= 17 || currentHour < 5) {
+      greet.value = 'Good evening!';
+    } else {
+      greet.value = 'Hello!';
+    }
+  } else {
+    if (!date) {
+      console.warn(`date is null!`);
+    } else if (!currentHour) {
+      console.warn(`time is null!`);
+    }
+  }
+});
+
+onBeforeUnmount(() => {
+  clearInterval(clockInterval);
+});
 </script>
 
 <template>
   <!-- Welcome! -->
   <div class="text-center mx-auto py-50px">
-    <p class="font-en font-600 text-60px text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">Welcome!</p>
+    <p class="font-en font-600 text-60px text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9 line-height-tight">{{ greet }}</p>
+    <p class="font-ja font-500 text-transparent bg-clip-text bg-gradient-to-b from-gray-6 to-gray-9">{{ clock }}</p>
   </div>
 
   <!-- Profile -->
