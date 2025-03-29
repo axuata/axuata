5c8f23c7a1d383aaf118631d7bddc6badedf49dc
Axuata <axuata@gmail.com>
2025-03-29 13:36:24 +0900

    Added tech section for posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,6 +9,11 @@
       <CLinkCard href="/posts/news/2025-03-25" emoji="✨" title="リニューアルのお知らせ" desc="2025/03/25" />
     </div>
   </CSection>
+  <CSection title="Tech" title-align="center">
+    <div class="font-en font-500 text-center">
+      <a href="https://zenn.dev/axuata">Zenn</a>
+    </div>
+  </CSection>
   <DevOnly>
     <CLinkCard href="/posts/formatest" emoji="✨" title="Formatterのテスト" desc="2025/03/26" />
   </DevOnly>
