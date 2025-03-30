4f455bb082b19bf4315a287ccf3fb4f89c3dd7ab
Axuata <axuata@gmail.com>
2025-03-30 21:30:09 +0900

    Changed license

--- components/layouts/CFooter.vue
+++ components/layouts/CFooter.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="py-20px">
-    <p class="color-gray-7 font-600 text-center">© 2025 Axuata - This project is licensed under the MIT License.</p>
+    <p class="color-gray-7 font-600 text-center">© 2025 Axuata - All Rights Reserved.</p>
   </div>
 </template>
 
