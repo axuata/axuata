3865715eba4b0ba4e76f211854106753ab77affc
Axuata <axuata@gmail.com>
2025-04-09 17:02:28 +0900

    Updated Icon

--- components/layouts/CHeader.vue
+++ components/layouts/CHeader.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex h-60px items-center justify-between">
     <a class="h-50px" href="/">
-      <NuxtImg src="/images/axuata/icon.png" alt="Axuata's Icon" class="size-50px" />
+      <NuxtImg src="/images/axuata/icon.svg" alt="Axuata's Icon" class="size-50px" />
     </a>
     <div class="*:mx-3px *:color-gray-7 *:font-500 *:no-underline">
       <a href="/" class="font-en hover:font-600 transition-all duration-100">Home</a>
